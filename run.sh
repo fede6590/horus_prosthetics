9f2a2bdf5815d53441cbe1ea2bf38b81b23604de
Federico Ferreyra <54560956+fede6590@users.noreply.github.com>
2024-01-18 16:09:37 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-# Set environment variables
-export FIRSTNAME=Federico
-export LASTNAME=Ferreyra
-
-# Define the Docker image name
-image_name="horus_app"
-
-# # Check if the Docker image exists
-# if [[ "$(docker images -q $image_name 2> /dev/null)" == "" ]]; then
-#     Build the Docker image from the Dockerfile
-#     docker build -t $image_name .
-# fi
-
-docker build -t $image_name .
-
-# Run the Docker container with the environment variables
-docker run -e FIRSTNAME -e LASTNAME -p 8501:8501 $image_name
+apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
